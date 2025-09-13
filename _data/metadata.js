1b4a876a848404782f87625ea4d1c40fa2d937ff
刘留 <2589924489@qq.com>
2025-09-13 11:24:38 +0800

    Add GitHub Pages deployment workflow

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
-module.exports = {
+export default {
 	title: "Liu Lewis Blog",
 	url: "https://liu7749.github.io/Lewis-blog/",
 	language: "zh-CN",
 	description: "I am writing about my experiences as a cs student.",
 	author: {
 		name: "Liu Liu",
-		studentemail: "20234080404@stu.usc.edu.cn",
+		studentEmail: "20234080404@stu.usc.edu.cn",
 		personalEmail: "happlyliuliu@qq.com",
 		url: "https://github.com/Liu7749"
 	}
